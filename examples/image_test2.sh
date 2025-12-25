9c16fb7a9383590910979474a3a25b747611fdee
Prima24 <primajuliano17@gmail.com>
2025-12-25 20:38:25 +0700

    style

--- examples/image_test2.sh
+++ examples/image_test2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-TASK_ID="b16114db-6e86-4684-8d26-c19160fb6a1f"
-MODEL="misri/zavychromaxl_v90"
-DATASET_ZIP="https://gradients.s3.eu-north-1.amazonaws.com/2939e90500591416_train_data.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVVZOOA7SA4UOFLPI%2F20251204%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20251204T000029Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=a57d16e23bcb6e94719580512b0f8ba7760c321852318277c83319155984768f"
+TASK_ID="636cbffb-4421-4a4a-8741-8f9b020cfe45"
+MODEL="John6666/hassaku-xl-illustrious-v10style-sdxl"
+DATASET_ZIP="https://gradients.s3.eu-north-1.amazonaws.com/09080e43bbe77575_train_data.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVVZOOA7SA4UOFLPI%2F20251221%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20251221T005957Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=85fbc542916e33e8dc57c57a339d9f83521066efc865f2ab1ab221a91ed770bd"
 MODEL_TYPE="sdxl"
-EXPECTED_REPO_NAME="b16114db-6e86-4684-8d26-c19160fb6a1f-repo1"
+EXPECTED_REPO_NAME="636cbffb-4421-4a4a-8741-8f9b020cfe45-repoxyz"
 
-HUGGINGFACE_TOKEN="Your Huggingface Token"
-HUGGINGFACE_USERNAME="Your Huggingface Username"
+HUGGINGFACE_TOKEN="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"
+HUGGINGFACE_USERNAME="Gege24"
 LOCAL_FOLDER="/app/checkpoints/$TASK_ID/$EXPECTED_REPO_NAME"
 
 CHECKPOINTS_DIR="$(pwd)/secure_checkpoints"
